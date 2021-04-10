fd2e7332474f60b6f7a33e0502ddad6d58be6ecb
Carley Engleson <cjengleson@gmail.com>
2021-04-10 16:41:21 -0700

    Completed Homework

--- PyBank/Resources/main.py
+++ PyBank/Resources/main.py
@@ -3,47 +3,47 @@ import os
 
 import csv
 
-#create path to csv
+#create path to budget csv
 pybank_csvpath = os.path.join("..", "Resources", "budget_data.csv")
 
-# set variables
+#set variables
 total_months = []
 net_profit = []
 monthly_profit_change = []
 overall_profit_change = 0
 
-# reading through budget data as csvreader
+#reading through budget data as csvreader
 with open(pybank_csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter = ",")
     next(csvreader, None) # skip header row
     
-    # read through each row of data
+    #read through each row of data
     for row in csvreader:
         total_months.append(row[0])
         net_profit.append(int(row[1]))
     
     for i in range(len(net_profit)-1):
-        # make a new list where following month (ie Feb 2010) - current month (ie January 2010)
-        # to find the monthly profit change
+        #make a new list where following month (ie Feb 2010) - current month (ie January 2010)
+        #to find the monthly profit change
         monthly_profit_change.append(net_profit[i+1]-net_profit[i])
 
-        # find average profit change
+        #find average profit change
         overall_profit_change = int(sum(monthly_profit_change)) / int(len(monthly_profit_change))
 
-        # format accordingly using 2 decimal places
+        #format accordingly using 2 decimal places
         formatted_profit_change = "{:.2f}".format(overall_profit_change)
 
 
-# find maximum/minimum monthly profit change        
+#find maximum/minimum monthly profit change        
 max_increase = max(monthly_profit_change)
 min_increase = min(monthly_profit_change)
 
-# associate month with monthly profit change using index function to find month where
-# profit change happened, + 1 to change month output to the next month
+#associate month with monthly profit change using index function to find month where
+#profit change happened, + 1 to change month output to the next month
 max_month = monthly_profit_change.index(max_increase) + 1
 min_month = monthly_profit_change.index(min_increase) + 1
 
-# set total net profit
+#set total net profit
 net_profit = int(sum(net_profit))
 
 print("----------------------------")
@@ -55,7 +55,7 @@ print("Average Change: " + str(formatted_profit_change))
 print(f"Greatest Increase in Profits: {total_months[max_month]} (${str(max_increase)})")  
 print(f"Greatest Decrease in Profits: {total_months[min_month]} (${str(min_increase)})")
 
-# export to text file
+#export to text file
 output_txt_file = open("../analysis/Budget_Analysis.txt", "w")
 output_txt_file.write("Financial Analysis")
 output_txt_file.write("\n")
---
+++ PyPoll/Resources/main.py
@@ -0,0 +1,105 @@
+#import os and csv
+import os
+
+import csv
+
+#create path to election data csv
+pypoll_csvpath = os.path.join("..", "Resources", "election_data.csv")
+
+#set variables
+
+#total votes
+total_votes = [] 
+
+#complete list of candidates who received votes
+khan_can = 0
+correy_can = 0
+li_can = 0
+otooley_can = 0
+
+#reading through election data as csvreader
+with open(pypoll_csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
+    next(csvreader, None) # skip header row
+
+    #read through each row of data
+    for row in csvreader:
+
+        #total number of votes
+        total_votes.append(row[0])
+
+
+        #winner of election based on popular vote
+
+
+        #total number votes each candidate won
+        if row[2] == "Khan":
+            khan_can = khan_can + 1
+        elif row[2] == "Correy":
+            correy_can = correy_can + 1
+        elif row[2] == "Li":
+            li_can = li_can + 1
+        elif row[2] == "O'Tooley": 
+            otooley_can = otooley_can + 1
+
+
+#winner of election based on popular vote
+#create dictionary of candidates and total votes per candidate
+#zip the two together to make a key that finds the max aka winner
+candidates = ["Khan", "Correy", "Li", "O'Tooley"]
+votes = [khan_can, correy_can, li_can, otooley_can]
+dict_candidates_and_votes = dict(zip(candidates,votes))
+key = max(dict_candidates_and_votes, key=dict_candidates_and_votes.get)
+
+#change list to integer to use in the percent votes calculation
+total_votes = int(len(total_votes))
+
+#percent votes each candidate won with formatting to single decimal point
+percent_khan = (khan_can / total_votes) * 100
+formatted_khan = str(("{:.4}".format(percent_khan)))
+
+percent_correy = (correy_can / total_votes) * 100
+formatted_correy = str(("{:.4}".format(percent_correy)))
+
+percent_li = (li_can / total_votes) * 100
+formatted_li = str(("{:.4}".format(percent_li)))
+
+percent_otooley = (otooley_can / total_votes) * 100
+formatted_otooley = str(("{:.4}".format(percent_otooley)))
+
+#print statements
+print("Election Results")
+print("----------------------------")
+print("Total Votes: " + str(total_votes))
+print("----------------------------")
+print("Khan: " + formatted_khan + "% " + str(khan_can))
+print("Correy: " + formatted_correy + "% " + str(correy_can))
+print("Li: " + formatted_li + "% " + str(li_can))
+print("O'Tooley: " + formatted_otooley + "% " + str(otooley_can))
+print("----------------------------")
+print("Winner: " + key)
+print("----------------------------")
+
+#export to text file
+poll_output_txt_file = open("../analysis/Poll_Analysis.txt", "w")
+poll_output_txt_file.write("Election Results")
+poll_output_txt_file.write("\n")
+poll_output_txt_file.write("----------------------------")
+poll_output_txt_file.write("\n")
+poll_output_txt_file.write("Total Votes: " + str(total_votes))
+poll_output_txt_file.write("\n")
+poll_output_txt_file.write("----------------------------")
+poll_output_txt_file.write("\n")
+poll_output_txt_file.write("Khan: " + formatted_khan + "% " + str(khan_can))
+poll_output_txt_file.write("\n")
+poll_output_txt_file.write("Correy: " + formatted_correy + "% " + str(correy_can))
+poll_output_txt_file.write("\n")
+poll_output_txt_file.write("Li: " + formatted_li + "% " + str(li_can))
+poll_output_txt_file.write("\n")
+poll_output_txt_file.write("O'Tooley: " + formatted_otooley + "% " + str(otooley_can))
+poll_output_txt_file.write("\n")
+poll_output_txt_file.write("----------------------------")
+poll_output_txt_file.write("\n")
+poll_output_txt_file.write("Winner: " + key)
+poll_output_txt_file.write("\n")
+poll_output_txt_file.write("----------------------------")
